5c5ee2a39075a680b56681dd0f06243424fa9994
Alpha <aykwong93@gmail.com>
2018-06-16 23:58:35 -0400

    Initial html, css setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+var trivia = {
+  questions: [1, 2, 3, 4],
+  Q1A: [1, 2, 3, 4],
+  Q2A: [1, 2, 3, 4],
+  Q3A: [1, 2, 3, 4],
+  Q4A: [1, 2, 3, 4]
+};
+
+var answers = {
+    Q1A: 1,
+    Q2A: 2,
+    Q3A: 3,
+    Q4A: 4
+}
+
+var intervalId
+
+$(document).on("click", function() {
+
+});

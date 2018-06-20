be547ce4a6be8693d5647e7f559a2393a5bfd273
Kwong <aykwong93@gmail.com>
2018-06-19 23:36:30 -0400

    Fixed event listeners

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,43 +43,33 @@ var stats = {
 var timer = {
   intervalId: "",
   time: 5,
-  timeReset: 0
+  timeReset: 0,
+  start: setInterval(function() {
+    $(this).time -= 1;
+    $("#timeLeft").html($(this).time);
+  }, 1000),
+  clear: function() {
+    clearInterval($(this).start);
+  }
 };
 
-let quizCount = 1;
+var quizCount = 1;
 
 $("button").on("click", function run() {
   //Hides the button and starts the game
   $("#preStart").css("display", "none");
   $("#postStart").css("display", "block");
 
-function timeKeeper() {
-  if(timer.time > 0 && timer.reset === 0) {
-    timer.intervalId = setInterval(function() {
-      timer.time -=1;
-      $("#timeLeft").html(timer.time);
-    }, 1000) } else if(timer.time <= 0) {
-      
-    } else {
-      clearInterval(timer.intervalId);
-      timer.reset = 0;
-      timer.time = 5;
-      timeKeeper();
-    }
-  }
-
-  timer.intervalId = setInterval(function () {
-    timer.time -= 1;
-    $("#timeLeft").html(timer.time);
-    if (timer.time === 0) {
-      clearInterval(timer.intervalId);
-    } else if(timer.reset === 1) {
-      clearInterval(timer.intervalId);
-      timer.reset = 0;
-      timer.time = 5;
-      timer.intervalId;
-    }
-  }, 1000);
+// function() {
+//   if(timer.time > 0) {
+//     timer.start;
+//   } else if (timer.reset === 1) {
+//       clearInterval(timer.intervalId);
+//       timer.reset = 0;
+//       timer.time = 5;
+//       timeKeeper();
+//     }
+//   }
 
   //Runs through the array of questions and dynamically generates the question and choices
   let count = "Q" + quizCount;
@@ -102,50 +92,64 @@ function timeKeeper() {
   });
 
   //Evaluates the user's choice and listens for a button click to reset for next question
-  $(".answers").on("click", ".choice", function () {
-    let result = $("<div>");
-    $(result).addClass("mb-4");
-
-    setTimeout(nextQuestion, 4000);
-    $(".question").html("<img src=" + trivia[count].picture + ' alt="' + trivia[count].answer + '" width="450px" />');
-    $(".answers").html("<button>Click to continue</button>");
-    if ($(this).text() === trivia[count].answer) {
-      $(result).html("You're right!");
-      stats.correct++;
-      $(".answers").prepend(result);
-    } else if ($(this).text() !== trivia[count].answer) {
-      $(result).html("You're wrong. The correct answer is: " + trivia[count].answer);
-      stats.incorrect++;
-      $(".answers").prepend(result);
-    }
+    $(".answers").on("click", ".choice", function answerPhase() {
+      let result = $("<div>");
+      $(result).addClass("mb-4");
+    
+      setTimeout(nextQuestion, 2000);
+      console.log("inside click: " + count);
+      $(".question").html("<img src=" + trivia[count].picture + ' alt="' + trivia[count].answer + '" width="450px" />');
+      if ($(this).text() === trivia[count].answer) {
+        $(result).html("You're right!");
+        stats.correct++;
+        $(".answers").html(result);
+      } else {
+        $(result).html("You're wrong. The correct answer is: " + trivia[count].answer);
+        stats.incorrect++;
+        $(".answers").html(result);
+      };
+    });
 
     //Resets the quiz and increases quizCount by one for next question
     function nextQuestion() {
       quizCount++;
       timer.reset = 1;
       $(".question").empty();
-      $(".answers").remove(".choice");
+      $(".answers").off("click", ".choice");
       $(".answers").empty();
       run();
     }
-  });
 
+  // timer.intervalId = setInterval(function () {
+  //   timer.time -= 1;
+  //   $("#timeLeft").html(timer.time);
+  //   if (timer.time === 0) {
+  //     clearInterval(timer.intervalId);
+  //     answerPhase();
+  //   } else if(timer.reset === 1) {
+  //     clearInterval(timer.intervalId);
+  //     timer.reset = 0;
+  //     timer.time = 5;
+  //     timer.intervalId;
+  //   }
+  // }, 1000);
+
+  function endResult() {
+    $(".question").append(
+      "<div>Questions answered correctly:" + stats.correct + "</div>"
+    );
+    $(".question").append(
+      "<div>Questions answered incorrectly:" + stats.incorrect + "</div>"
+    );
+    $(".answers").html("<button>Click to try again</button>");
+  
+    $("button").on("click", function () {
+      quizCount = 1;
+      stats.correct = 0;
+      stats.incorrect = 0;
+      $(".question").empty();
+      $(".answers").empty();
+      run();
+    });
+  };
 });
-
-function endResult() {
-  $(".question").append(
-    "<div>Questions answered correctly:" + stats.correct + "</div>"
-  );
-  $(".question").append(
-    "<div>Questions answered incorrectly:" + stats.incorrect + "</div>"
-  );
-  $(".answers").html("<button>Click to try again</button>");
-  $("button").on("click", function () {
-    quizCount = 1;
-    stats.correct = 0;
-    stats.incorrect = 0;
-    $(".question").empty();
-    $(".answers").empty();
-    run();
-  });
-};
